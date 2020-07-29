3e6d36509b2c2919dbbf33cb84d64df96f871d13
Antoine Bolvy <antoine@labelleassiette.com>
2020-07-29 17:46:25 +0200

    Fix forgotten export

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = {
   rules: {
     'mongoose-exec': require('./rules/mongoose-exec'),
+    'mongoose-deprecated': require('./rules/mongoose-deprecated'),
   },
   rulesConfig: {
     'mongoose-exec': 0,
+    'mongoose-deprecated': 0,
   }
 };
